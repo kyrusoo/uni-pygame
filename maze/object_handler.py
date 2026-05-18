bdadfe2441485d119b82b5c5fdd865cc2ff76f18
kyrusoo <duzeldikovb@bk.ru>
2026-05-18 11:50:32 +0500

    feat: object_handler

---
+++ maze/object_handler.py
@@ -0,0 +1,27 @@
+from sprite_object import *
+
+
+
+class ObjectHandler:
+    def __init__(self, game):
+        self.game = game
+        self.sprite_list = []
+        self.static_sprite_path = 'resources/sprites/static_sprites/'
+        self.anim_sprite_path = 'resources/sprites/animated_sprites/'
+        add_sprite = self.add_sprite
+
+        # sprite map
+        add_sprite(SpriteObject(game))
+        # add_sprite(AnimatedSprite(game))
+        add_sprite(AnimatedSprite(game, pos=(1.5, 1.5)))
+        add_sprite(AnimatedSprite(game, pos=(1.5, 7.5)))
+        add_sprite(AnimatedSprite(game, pos=(5.0, 3.9), shift=-11, scale=0.1))
+        add_sprite(AnimatedSprite(game, pos=(5.0, 4.15), shift=-11, scale=0.1))
+        add_sprite(AnimatedSprite(game, pos=(14.5, 1.5)))
+        add_sprite(AnimatedSprite(game, pos=(14.5, 7.8)))
+
+    def update(self):
+        [sprite.update() for sprite in self.sprite_list]
+
+    def add_sprite(self, sprite):
+        self.sprite_list.append(sprite)
