dc5dd3e589d2a73314c24de4ffb65b972ddbcd56
kyrusoo <duzeldikovb@bk.ru>
2026-05-18 23:31:47 +0500

    refactor: moved healthbar to the left bottom corner

--- maze/object_renderer.py
+++ maze/object_renderer.py
@@ -26,8 +26,8 @@ class ObjectRenderer:
     def draw_player_health(self):
         health = str(self.game.player.health)
         for i, char in enumerate(health):
-            self.screen.blit(self.digits[char], (i * self.digit_size, 0))
-        self.screen.blit(self.digits['10'], ((i+1) * self.digit_size, 0))
+            self.screen.blit(self.digits[char], (i * self.digit_size, HEIGHT*0.85))
+        self.screen.blit(self.digits['10'], ((i+1) * self.digit_size, HEIGHT*0.85))
 
     def player_damage(self):
         self.screen.blit(self.blood_screen, (0, 0))
