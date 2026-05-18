8562155fa360df7ef23fde0b8644ef3bd7c85ec5
kyrusoo <duzeldikovb@bk.ru>
2026-05-18 11:29:53 +0500

    feat: animated_sprite(restless__eye)

--- maze/sprite_object.py
+++ maze/sprite_object.py
@@ -1,9 +1,12 @@
 import pygame as pg
 from settings import *
+import os
+from collections import deque
+
 
 class SpriteObject:
     def __init__(self, game, path='resources/sprites/static_sprites/1.png',
-                 pos=(5, 4.0), scale=1.5, shift=-0.5):
+                 pos=(5.0, 4.0), scale=1.5, shift=-0.5):
         self.game = game
         self.player = game.player
         self.x, self.y = pos
@@ -48,3 +51,39 @@ class SpriteObject:
 
     def update(self):
         self.get_sprite()
+
+
+class AnimatedSprite(SpriteObject):
+    def __init__(self, game, path='resources/sprites/animated_sprites/eye/eye-1.png',
+                 pos=(5.8, 4.0), scale=0.8, shift=0.0, animation_time=120):
+        super().__init__(game, path, pos, scale, shift)
+        self.animation_time = animation_time
+        self.path = path.rsplit('/', 1)[0]
+        self.images = self.get_images(self.path)
+        self.animation_time_prev = pg.time.get_ticks()
+        self.animation_trigger = False
+
+    def update(self):
+        super().update()
+        self.check_animation_time()
+        self.animate(self.images)
+
+    def animate(self, images):
+        if self.animation_trigger:
+            images.rotate(-1)
+            self.image = images[0]
+
+    def check_animation_time(self):
+        self.animation_trigger = False
+        time_now = pg.time.get_ticks()
+        if time_now - self.animation_time_prev > self.animation_time:
+            self.animation_time_prev = time_now
+            self.animation_trigger = True
+
+    def get_images(self, path):
+        images = deque()
+        for file_name in os.listdir(path):
+            if os.path.isfile(os.path.join(path, file_name)):
+                img = pg.image.load(path + '/' + file_name).convert_alpha()
+                images.append(img)
+        return images
