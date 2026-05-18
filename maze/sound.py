8965b32323e309b783faf640b44debc37531da87
kyrusoo <duzeldikovb@bk.ru>
2026-05-18 20:01:13 +0500

    feat: npc, shot, theme sounds

--- maze/sound.py
+++ maze/sound.py
@@ -7,3 +7,9 @@ class Sound:
         pg.mixer.init()
         self.path = 'resources/sound/'
         self.finger = pg.mixer.Sound(self.path + 'finger.wav')
+        self.npc_pain = pg.mixer.Sound(self.path + 'npc_pain.wav')
+        self.npc_death = pg.mixer.Sound(self.path + 'npc_death.wav')
+        self.npc_shot = pg.mixer.Sound(self.path + 'npc_shot.wav')
+        self.player_pain = pg.mixer.Sound(self.path + 'player_pain.wav')
+        self.theme = pg.mixer.Sound(self.path + 'theme.mp3')
+        
